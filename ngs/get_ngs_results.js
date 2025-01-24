b35577ade9449b5e4acd02a141b8795095bfd9a9
Rowan Christmas <rowanxmas@gmail.com>
2025-01-24 14:10:53 -0800

    update

--- ngs/get_ngs_results.js
+++ ngs/get_ngs_results.js
@@ -55,40 +55,5 @@ async function start() {
     storeData(all_teams, `ngs_archive/ngs_s${season}_results.json`)
 
 
-    // let post_all =  _.map(last_season_mongo, async (div) => {
-    //     // console.log(div)
-
-    //     return await api.post('/standings/fetch/division', { "division": div.object.divisionConcat, "season": season, "pastSeason": true })
-    //         .then((response) => {
-    //             // console.log(JSON.stringify(response.data), null, 4)
-    //             let data
-    //             try {
-    //                  data = response.data.returnObject
-    //             } catch (e) {
-    //                 console.log('no data')
-    //                 console.log(e.error)
-    //                 return
-    //             }
-                
-                
-    //             for (let i = 0; i < data.length; i++) {
-    //                 let team = data[i]
-    //                 if (team.teamName.endsWith('(Withdrawn)')) {
-    //                     team['withdrawn'] = true
-    //                     team.teamName = team.teamName.replace(' (Withdrawn)', '')
-    //                 }
-    //                 team[`season_${season}_div`] = div.object.divisionConcat
-    //             }
-    //             let teams = _.keyBy(data, 'teamName')
-    //             Object.assign(all_teams, teams)
-    //             console.log('added teams')
-    //         })
-
-    // })
-    // Promise.all(post_all).then( () => {
-    //     // console.log(all_teams)
-    //     storeData(all_teams, `ngs_archive/ngs_s${season}_results.json`)
-    // })
-
 }
 start().catch(console.error)
