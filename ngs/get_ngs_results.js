1f8451ed460a93cd463aeafad2fdef2d6bcca2cf
Rowan Christmas <rowanxmas@gmail.com>
2025-01-24 14:06:46 -0800

    seaon 19 updates

--- ngs/get_ngs_results.js
+++ ngs/get_ngs_results.js
@@ -14,7 +14,6 @@ const season = process.env.THIS_SEASON - 1
 let last_season_mongo = JSON.parse(fs.readFileSync(`ngs_archive/ngs_s_${season}.json`, 'utf8'))
 
 
-let all_teams = {}
 
 const storeData = (data, path) => {
     try {
@@ -26,33 +25,70 @@ const storeData = (data, path) => {
 
 async function start() {
 
-    let post_all =  _.map(last_season_mongo, async (div) => {
-        // console.log(div)
+    let divs = _.chain(last_season_mongo)
+    .map('object.divisionConcat')
+    .filter()
+    .uniq()
+    .value()
 
-        return await api.post('/standings/fetch/division', { "division": div.object.divisionConcat, "season": season, "pastSeason": true })
-            .then((response) => {
-                // console.log(JSON.stringify(response.data), null, 4)
+    let all_teams = {}
 
-                let data = response.data.returnObject
+    for (let i = 0; i < divs.length; i++) {
+        let div = divs[i]
+        let response = await api.post('/standings/fetch/division', { "division": div, "season": season, "pastSeason": true })
+        let data = response.data.returnObject
+        if (!data) {
+            continue
+        }
+        for (let i = 0; i < data.length; i++) {
+            let team = data[i]
+            if (team.teamName.endsWith('(Withdrawn)')) {
+                team['withdrawn'] = true
+                team.teamName = team.teamName.replace(' (Withdrawn)', '')
+            }
+            team[`season_${season}_div`] = div
+        }
+        let teams = _.keyBy(data, 'teamName')
+        Object.assign(all_teams, teams)
+    }
+
+    storeData(all_teams, `ngs_archive/ngs_s${season}_results.json`)
+
+
+    // let post_all =  _.map(last_season_mongo, async (div) => {
+    //     // console.log(div)
+
+    //     return await api.post('/standings/fetch/division', { "division": div.object.divisionConcat, "season": season, "pastSeason": true })
+    //         .then((response) => {
+    //             // console.log(JSON.stringify(response.data), null, 4)
+    //             let data
+    //             try {
+    //                  data = response.data.returnObject
+    //             } catch (e) {
+    //                 console.log('no data')
+    //                 console.log(e.error)
+    //                 return
+    //             }
                 
-                for (let i = 0; i < data.length; i++) {
-                    let team = data[i]
-                    if (team.teamName.endsWith('(Withdrawn)')) {
-                        team['withdrawn'] = true
-                        team.teamName = team.teamName.replace(' (Withdrawn)', '')
-                    }
-                    team[`season_${season}_div`] = div.object.divisionConcat
-                }
-                let teams = _.keyBy(data, 'teamName')
-                Object.assign(all_teams, teams)
-                console.log('added teams')
-            })
+                
+    //             for (let i = 0; i < data.length; i++) {
+    //                 let team = data[i]
+    //                 if (team.teamName.endsWith('(Withdrawn)')) {
+    //                     team['withdrawn'] = true
+    //                     team.teamName = team.teamName.replace(' (Withdrawn)', '')
+    //                 }
+    //                 team[`season_${season}_div`] = div.object.divisionConcat
+    //             }
+    //             let teams = _.keyBy(data, 'teamName')
+    //             Object.assign(all_teams, teams)
+    //             console.log('added teams')
+    //         })
 
-    })
-    Promise.all(post_all).then( () => {
-        // console.log(all_teams)
-        storeData(all_teams, `ngs_archive/ngs_s${season}_results.json`)
-    })
+    // })
+    // Promise.all(post_all).then( () => {
+    //     // console.log(all_teams)
+    //     storeData(all_teams, `ngs_archive/ngs_s${season}_results.json`)
+    // })
 
 }
 start().catch(console.error)
